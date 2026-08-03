a1cf4d7f5a4c8a9f3083f695289ce83f79deed5d
서현 <00a0s0d0f00@gmail.com>
2026-08-03 22:03:39 +0900

    Feat: 숫자 입력 함수 구현

---
+++ QUIZ.py
@@ -0,0 +1,45 @@
+import sys #KeyboardInterrupt와 EOFError 처리용 시스템 
+
+# 1. 메뉴(로 복귀도 가능해야하고 메뉴에서 나갈수도있어야하고나너무배고프다그냥재미없지나갈까..상태가되)
+
+# 2. 입력/예외처리 함수 
+# 숫자 입력(앞뒤 공백 제거, 변환 실패 or 범위밖 or 빈입력 시 재입력)
+def numinput(min=1, max=4): #default 범위가 1~4(답안 선택지)
+    while True:
+        try:
+            num = input("선택할 번호를 입력해주세요: ")
+            num = num.strip()
+            
+            #공백 체크
+            if num == "":            
+                print("입력이 비어 있습니다. 다시 입력하세요.")
+                continue
+            value = int(num)
+
+            # 범위 체크
+            if value < 1:
+                print(f"{min} 이상의 값을 입력해주세요.")
+                continue
+            if value > 4:
+                print(f"{max} 이히의 값을 입력해주세요.")
+                continue
+            return value
+
+        except ValueError:
+            # 정수 체크
+            print("정수를 입력해주세요.")
+        except (KeyboardInterrupt, EOFError):
+            # 에러 발생시 
+            print("\n입력이 취소되었습니다. 프로그램을 종료합니다.")
+            # 필요한 저장 작업이 있다면 여기에서 수행
+            sys.exit(0)
+# 데이터 파일이 없거나, 손상된 경우 -> 기본 퀴즈 데이터로 복구(혹은 초기화)
+
+#퀴즈 클래스
+
+#퀴즈 추가
+#퀴즈 목록
+#퀴즈 풀기
+#점수 확인
+#퀴즈게임 클래스
+#파일 저장 및 불러오기
